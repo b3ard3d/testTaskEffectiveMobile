ae78040e15e644effddbf5bb0a8d3ee78486d14f
Roman <rmn-08@yandex.ru>
2024-08-27 09:20:48 +0500

    coreData and network

---
+++ testTaskEffectiveMobile/DataBaseModels/TodosEntity.swift
@@ -0,0 +1,8 @@
+//
+//  TodosEntity.swift
+//  testTaskEffectiveMobile
+//
+//  Created by Роман Кокорев on 26.08.2024.
+//
+
+import Foundation
---
+++ testTaskEffectiveMobile/Models/Todos.swift
@@ -0,0 +1,8 @@
+//
+//  Todos.swift
+//  testTaskEffectiveMobile
+//
+//  Created by Роман Кокорев on 26.08.2024.
+//
+
+import Foundation
